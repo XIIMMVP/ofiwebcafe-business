fb28d89946ab65ac7e5de5c9c80e3591065cc9ac
n0do4lpha <xi.i.mmv.p@MacBook-Air-de-XIIMMVP.local>
2026-03-20 19:51:36 +0000

    Añadidos nuevos puntos de entrada HTML a vite.config.js para que se incluyan en el build

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,8 @@ export default defineConfig({
         about: resolve(__dirname, 'about.html'),
         services: resolve(__dirname, 'services.html'),
         contact: resolve(__dirname, 'contact.html'),
+        'politica-de-privacidad': resolve(__dirname, 'politica-de-privacidad.html'),
+        'terminos-y-condiciones': resolve(__dirname, 'terminos-y-condiciones.html'),
       },
     },
   },
